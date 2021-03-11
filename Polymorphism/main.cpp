b45bf854b420b2ec38d8ae4f5456eef135aecad8
averyle <ngangianggg@gmail.com>
2021-03-10 22:43:07 -0800

    Step 2

--- Polymorphism/main.cpp
+++ Polymorphism/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 #include <string>
 
+class Shape {};
+
+class Rectangle : public Shape {
+public:
+    double _width;
+    double _height;
+    Rectangle(double width, double height) {
+        _width = width;
+        _height = height;
+    }
+};
+
+class Square : public Shape {
+public:
+    double _side;
+    Square(double side) {
+        _side = side;
+    }
+};
+
+class Circle : public Shape {
+public:
+    double _radius;
+    Circle(double radius) {
+        _radius = radius;
+    }
+};
+
 int main() {
-	std::cout << "Hello, World";
+    Rectangle r1{ 3.0, 5.0 };
+    Square s1{ 4.0 };
+    Circle c1{ 10.0 };
+    // printArea(r1);
+    // printArea(s1);
+    // printArea(c1);
 }
