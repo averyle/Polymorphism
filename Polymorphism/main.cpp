c4e4fe2634bd4a72a9797c93f147a69ba9240d2a
averyle <ngangianggg@gmail.com>
2021-03-10 23:43:47 -0800

    Section 3

--- Polymorphism/main.cpp
+++ Polymorphism/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <string>
 
-class Shape {};
+class Shape {
+public:
+    double _area = 0.0;
+    virtual double area() const;
+};
+
+double Shape::area() const {
+    return _area;
+}
 
 class Rectangle : public Shape {
 public:
@@ -11,7 +19,12 @@ public:
         _width = width;
         _height = height;
     }
+    double area() const override;
 };
+
+double Rectangle::area() const {
+    return _width * _height;
+}
 
 class Square : public Shape {
 public:
@@ -19,7 +32,12 @@ public:
     Square(double side) {
         _side = side;
     }
+    double area() const override;
 };
+
+double Square::area() const {
+    return _side * _side;
+}
 
 class Circle : public Shape {
 public:
@@ -27,13 +45,22 @@ public:
     Circle(double radius) {
         _radius = radius;
     }
+    double area() const override;
 };
+
+double Circle::area() const {
+    return _radius * _radius * 3.14;
+}
+
+void printArea(const Shape& shape) { 
+    std::cout << shape.area() << "\n"; 
+}
 
 int main() {
     Rectangle r1{ 3.0, 5.0 };
     Square s1{ 4.0 };
     Circle c1{ 10.0 };
-    // printArea(r1);
-    // printArea(s1);
-    // printArea(c1);
+    printArea(r1);
+    printArea(s1);
+    printArea(c1);
 }
