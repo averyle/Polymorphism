e3e8d5a6a184ea50baeadb312152498ab0493cd6
averyle <ngangianggg@gmail.com>
2021-03-13 01:06:40 -0800

    Step 6

--- Polymorphism/main.cpp
+++ Polymorphism/main.cpp
@@ -56,3 +56,14 @@ int main() {
     printArea(s1);
     printArea(c1);
 }
+
+/*
+1. Which one of these do you think is easier to implementand use, dynamic or static polymorphism ?
+I like static polymorphism better because it is faster, more precise, and easy to declare. 
+
+2. Modern C++ development highly encourages static polymorphism over dynamic polymorphism, why do you think that is ?
+I think static polymorphism is preferred because you don't have to go through the process of writing codes in the
+superclass just to override it in subclasses. With static polymorphism, you can declare a generic funtion type,
+and the compiler will automatically assign whatever type the value is being passed as the type being used. In a way,
+T is just an object type, like one you would have if you create a class object.
+*/
